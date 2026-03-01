dd55262afd5f1b59b444948e6cf833442d478734
Saad Hussain <saadcfi4@gmail.com>
2026-03-01 04:50:40 +0400

    L1- 31:01

---
+++ hello.py
@@ -0,0 +1,8 @@
+# python file_name is the command to run the code
+
+# Ask user for their name
+name = input("What's your name? ")
+
+# Say hello to user
+# When you use , it automatically adds a blank space for you
+print("Hello,", name)
