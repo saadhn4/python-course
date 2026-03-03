b34097ef9e43860a6443e106f94311efc4b7c498
Saad Hussain <saadcfi4@gmail.com>
2026-03-03 04:27:00 +0400

    float, numeric formatting, functions, return value

--- 2. calculator.py
+++ 2. calculator.py
@@ -1,5 +1,25 @@
-# Converting the strings into integers
-x = int(input('Enter x: '))
-y = int(input('Enter y: '))
+# Converting the strings into float/integers
+# x = float(input('Enter x: '))
+# y = float(input('Enter y: '))
 
-print(f'The sum of {x} and {y} is {x+y}')
+# Rounds to the number if its .5 or more
+# z = round(x+y)
+
+# Adds comma for 1,000/1,000,000 etc.
+# print(f"{z:,}")
+
+# Rounds to 2 decimal places
+# w = round(x/y, 2)
+# print(w)
+
+# Function that accepts 2 numbers and squares them
+def main():
+  x = int(input('Enter c: '))
+  y = int(input('Enter d: '))
+  print(f"{x} squared is {square(x)}")
+  print(f"{y} squared is {square(y)}")
+  
+def square(value):
+  return value ** 2
+
+main()
