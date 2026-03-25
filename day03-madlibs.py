eacd006b193c5c4dcd85fe4ae8f388ccbfbf7636
Saad Hussain <saadcfi4@gmail.com>
2026-03-26 02:57:06 +0400

    madlibs game

---
+++ day03-madlibs.py
@@ -0,0 +1,14 @@
+# Madlibs game
+# Game where you create a story 
+# by filling in blanks with random words
+
+adjective1 = input('Enter an adjective (desc): ')
+noun1 = input('Enter a noun (person, place or thing): ')
+adjective2 = input('Enter an adjective (desc): ')
+verb1 = input('Enter a verb ending with ing: ')
+adjective3 = input('Enter an adjective (desc): ')
+
+print(f"Today i went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
