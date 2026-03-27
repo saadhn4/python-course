543fe470729d055729fd2c1c00f66dec748bd317
Saad Hussain <saadcfi4@gmail.com>
2026-03-27 09:31:38 +0400

    calculator program chanes

--- day07-calculator.py
+++ day07-calculator.py
@@ -1,18 +1,18 @@
-num_1 = float(input("Enter first number: "))
-num_2 = float(input("Enter second number: "))
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 operation = input("Enter the operation you want to do: ")
 
 if operation == '+':
-  result = num_1 + num_2
+  result = num1 + num2
   print(round(result,2))
 elif operation == '-':
-  result = num_1 - num_2
+  result = num1 - num2
   print(round(result,2))
 elif operation == '*':
-  result = num_1 * num_2
+  result = num1 * num2
   print(round(result,2))
 elif operation == '/':
-  result = num_1 / num_2
+  result = num1 / num2
   print(round(result,2))
 else:
   print('Not a valid operation.')
