9e0aeaff5aa5a7dc7081c1f608c45a840bad2a88
Saad Hussain <saadcfi4@gmail.com>
2026-03-28 06:12:57 +0400

    formatted with black

--- day09-temp-converter.py
+++ day09-temp-converter.py
@@ -1,11 +1,11 @@
-temp = int(input('Enter temperature: '))
-unit = input('Enter unit (f/c): ')
+temp = int(input("Enter temperature: "))
+unit = input("Enter unit (f/c): ")
 
-if unit == 'c':
-  temp = temp * 1.8 + 32
-  print(f'Temperature is {round(temp,2)}F')
-elif unit == 'f':
-  temp = (temp - 32) / 1.8
-  print(f'Temperature is {round(temp,2)}C')
+if unit == "c":
+    temp = temp * 1.8 + 32
+    print(f"Temperature is {round(temp,2)}F")
+elif unit == "f":
+    temp = (temp - 32) / 1.8
+    print(f"Temperature is {round(temp,2)}C")
 else:
-  print(f"{unit} is not a proper temperature unit")
+    print(f"{unit} is not a proper temperature unit")
