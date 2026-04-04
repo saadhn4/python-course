55922adcaedd2f8e6a5ad3b30021c6ee072d0086
Saad Hussain <saadcfi4@gmail.com>
2026-04-04 17:21:47 +0400

    while loops and practice

---
+++ day15-while-loops.py
@@ -0,0 +1,36 @@
+# while loop = execute some code WHILE some condition remains true
+
+# name = input("Enter name: ")
+
+# while name == "":
+#     print("You did not enter your name")
+
+#     # if there was no escape condition here, itd be infinite loop
+#     name = input("Enter name: ")
+
+# print(f"Hello {name}")
+
+# age = int(input("Enter your age: "))
+
+# # this can also be written as not age > 0
+# while  age < 0:
+#     print("You cannot be less than 1 year(s) old")
+#     age = int(input("Enter your age: "))
+
+# print(f"You are {age} years old")
+
+# food = input("Enter a food you like (q to quit): ")
+
+# while not food == "q":
+#     print(f"You like {food}")
+#     food = input("Enter another food you like (q to quit): ")
+
+# print("bye")
+
+# num = int(input("Enter a number between 1 and 10: "))
+
+# while num < 1 or num > 10:
+#     print(f"{num} isn't between 1 and 10")
+#     num = int(input("Enter a number between 1 and 10: "))
+
+# print(f"Your number is {num}")
--- practice.py
+++ practice.py
@@ -11,17 +11,76 @@
 # Email must end with ".com"
 # Email should not be only numbers
 
-email = input("Enter your new email: ")
+# email = input("Enter your new email: ")
 
-if len(email) > 20:
-    print("Your email must be less than or equal to 20 characters")
-elif not email.find(" ") == -1:
-    print("Email cannot contain spaces")
-elif email.count("@") != 1:
-    print("Email can contain only one @")
-elif not email.endswith(".com"):
-    print("Email must end with .com")
-elif email.isdigit():
-    print("Email must not only be numerals")
-else:
-    print(f"Hi {email}")
+# if len(email) > 20:
+#     print("Your email must be less than or equal to 20 characters")
+# elif not email.find(" ") == -1:
+#     print("Email cannot contain spaces")
+# elif email.count("@") != 1:
+#     print("Email can contain only one @")
+# elif not email.endswith(".com"):
+#     print("Email must end with .com")
+# elif email.isdigit():
+#     print("Email must not only be numerals")
+# else:
+#     print(f"Hi {email}")
+
+# While loop
+
+# 1. Password Check
+
+# Ask the user to enter a password.
+# Keep asking until the password is at least 6 characters long.
+
+# password = input("Enter your password (atleast 6 characters): ")
+
+# while len(password) < 6:
+#     print("Password's length is too short")
+#     password = input("Enter another password: ")
+
+# print(f"Your password is {password}")
+
+# 2. Positive Number Only
+
+# Ask for a number.
+# Keep asking until the number is greater than 0.
+
+# num = int(input("Enter a number greather than 0: "))
+
+# while num <= 0:
+#     print(f"{num} is less than 0")
+#     num = int(input("Enter a number greather than 0: "))
+
+# print(f"Your number is {num}")
+
+# 3. No Spaces Allowed
+
+# Ask for a username.
+# Keep asking if it contains a space.
+
+# username = input("Enter a username: ")
+
+# while not username.find(" ") == -1:
+#     print("Username cannot contain spaces")
+#     username = input("Enter another username: ")
+
+# print(f"Your username is: {username}")
+
+# 4. Valid Email Input
+
+# Ask for an email until:
+
+# No spaces
+# Contains exactly one @
+# Ends with .com
+
+email = input("Enter an email: ")
+
+while not email.find(" ") == -1 or email.count("@") != 1 or not email.endswith(".com"):
+    print(
+        "Email must not contain spaces or more than 1 @ and cannot end with anything other than .com"
+    )
+    email = input("Enter another email: ")
+
+print(f"Your email is {email}")
