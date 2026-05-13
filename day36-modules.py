72c85008dcc581fbd52117da727f1befb17d11a6
Saad Hussain <saadcfi4@gmail.com>
2026-05-14 02:03:25 +0400

    modules

---
+++ day36-modules.py
@@ -0,0 +1,19 @@
+# created a file called example.py for this
+import example
+
+# module = a file containing code you want to include in your own program
+
+# use import to include a module
+
+# useful to breakup a large program reusable seperate files
+
+# import math as haggu
+
+# from math import pi -> dont use this to avoid name conflicts
+
+# print(haggu.pi)
+# print(pi)
+
+result = example.cube(2)
+
+print(result)
---
+++ example.py
@@ -0,0 +1,17 @@
+pi = 3.14159
+
+
+def square(n):
+    return n * n
+
+
+def cube(n):
+    return n**3
+
+
+def circumference(r):
+    return 2 * pi * r
+
+
+def area(r):
+    return pi * r**2
